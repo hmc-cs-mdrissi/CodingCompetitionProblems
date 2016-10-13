d6b08178243a647f275ebee9e4397b04af39cee7
Mehdi Drissi <mdrissi@cs.hmc.edu>
2016-10-12 22:31:24 -0700

    Update 69_sqrt.rb
    Correcting a comment

--- LeetCode/69_sqrt.rb
+++ LeetCode/69_sqrt.rb
@@ -4,7 +4,8 @@ def my_sqrt(x)
     return 0 if x == 0
     current_guess = x/2.to_f
     
-    #This implements Newton's method to approximate the square root.
+    #This finds the average of the current_guess and x/current_guess
+    #to get closer to the actual answer.
     until((x - current_guess**2).abs < 0.5)
         current_guess = (x/current_guess + current_guess)/2
     end
